18a6626d07941d53e9a910868d5c3dfb33075879
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2023-08-24 23:57:58 -0500

    proyect creado

---
+++ soluLab2/src/main/java/com/example/solulab2/SoluLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.solulab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoluLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoluLab2Application.class, args);
+    }
+
+}
